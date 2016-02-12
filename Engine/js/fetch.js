e9e79e092015cac834857dfaa2e2192d958abb8c
Anurag Lingam <anuten8@gmail.com>
2016-02-12 22:58:38 +0530

    Added a chrome shortcut to enable CORS for fetching required JS during runtime. The relevant "fetch module" script called fetch.js is also ready. When supplied with a proper module name, this function will go to the folder that's named after the module and get the config.json file in the folder which contains the relative path to the module's latest js file. fetch.js will then simply parse the JS, which can be used on the caller window.

--- Engine/js/fetch.js
+++ Engine/js/fetch.js
@@ -5,36 +5,49 @@ function fetch(module,absolutePath) {
 	Each module will have a config file that specifies which function must be called.
 	Fetch will now call that particular function and return whatever data that function returns.
 	STATUS: Unfinished. chrome's policy does not allow xhr get on files that are stored locally. :(
+	WORKAROUND: Using --disable-web-security flag for now, which is not recommended.
 	*/
 	absolutePath = absolutePath||false;
-	var append = "\config.js";
+	var append = "/config.json";
+	var prepend = "../Modules/"
 	if (absolutePath===true){
-		append = "";
+		prepend = append = "";
 	}
 	var file;
-	if ((file=getFile(module+append))===true)
-		alert("Got the module");
+	var filePath = prepend+module+append;
+	file = getFile(filePath);
+	if (file)
+		console.log("Fetched the module",module);
 	else
-		alert("There's no such module");
+		console.error("There's no module called",module);
 }
 
 function getFile(fileLoc)
 {
-    var xmlhttp = new window.XMLHttpRequest();
-    try {
-        xmlhttp.open("GET", fileLoc, false);
-        xmlhttp.onreadystatechange=function() {
-            if(xmlhttp.readyState === 4)
-        {
-            if(xmlhttp.status === 200 || xmlhttp.status == 0)
-            {
-                return xmlhttp.responseText;
-            }
-        }
-        }
-        xmlhttp.send(null);
-    }
-    catch(ex) {
-        return false;
-    }
+	var xmlhttp = new window.XMLHttpRequest();
+	content=null;
+	console.log(fileLoc);
+	try {
+		xmlhttp.onreadystatechange=function() {
+			if(xmlhttp.readyState === 4)
+			{
+				if(xmlhttp.status === 200 || xmlhttp.status == 0)
+				{
+					content = JSON.parse(xmlhttp.responseText);
+					var url = fileLoc.split("/");
+					url.pop();
+					url = url.join("/")+"/"+content['fetch'];
+					$.getScript(url);
+				}
+			}
+		}
+		//Async parameter is false. Don't proceed unless module is fetched.
+		xmlhttp.open("GET", fileLoc, false);
+		xmlhttp.send();
+		return content;
+	}
+	catch(ex) {
+		console.error(ex);
+		return false;
+	}
 } 
