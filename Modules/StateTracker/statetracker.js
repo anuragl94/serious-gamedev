6c5a4f2761b58df80b5b6e39f71b61a883bb86c1
Anjana Menon <anj4494@gmail.com>
2016-02-28 12:34:11 +0530

    Modified statetracker module

--- Modules/StateTracker/statetracker.js
+++ Modules/StateTracker/statetracker.js
@@ -1,31 +1,44 @@
-question = document.getElementById("question").value();
-clues = document.getElementsByClassName("clues");
-currentClue = document.getElementById("currentClue").value();
-currOptions = document.getElementByClassName("options");
-states = document.getElementByClassName("state");
-options = document.getElementById("options").value();
-batteryState = document.getElementById("batteryState").value();
+<!DOCTYPE html>
+<html>
+	<head>
+	<script>
+			src = "jquery.min.js"; //put correct path
+	</script>
+	<script>
+			var question = $("#question").val();
+			var clues[] = $(".clues").val(); //multiple clues,how to store and parse
+			var currentClue = $("#currentClue").val();
+			var currOptions[] = $(".options").val();
+			var states = $(".state").val();
+			var options[] = $("#options").val();
+			var batteryState = $("#batteryState").val();
 
-var configText = JSON.stringify(currOptions); //not sure if I need to do these
-var stateText = JSON.stringify(states);
-var clueText = JSON.stringify(clues);
+			var configText = JSON.stringify(currOptions); //not sure if I need to do these
+			var stateText = JSON.stringify(states);
+			var clueText = JSON.stringify(clues);
 
-var staticParameters = [];
-var dynamicParameters = [];
+			var staticParameters = [];
+			var dynamicParameters = [];
 
-var staticParameters = {
-        "question": question,
-        "clues": clues
-    } ;
+			var staticParameters = {
+					"question": question,
+					"clues": clues
+				} ;
+				
+			var dynamicParamaters = {
+					"options": configText,
+					"states": stateText,
+					"currentClue": currentClue ,
+					"batteryState" : batteryState 
+				}
+				
+			var Aggregate = [staticParameters,dynamicParameters] ;
+
+			localStorage.setItem("stateVariables", JSON.stringify(Aggregate));
+
+			//jquery
+			//$(".classname").css()
+			//$(document).on() IIFE
+			//$("#w3s").attr("href") for attributes 
+	</script>	
 	
-var dynamicParamaters = {
-        "options": configText,
-        "states": stateText,
-        "currentClue": currentClue ,
-		"batteryState" : batteryState 
-    }
-	
-var Aggregate = [staticParameters,dynamicParameters] ;
-
-localStorage.setItem("stateVariables", JSON.stringify(Aggregate));
-
