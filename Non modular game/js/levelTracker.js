2ab56dbb50d392583a3cf80cbd55dfe061c385b1
Anurag Lingam <anurag.lingam@outlook.com>
2016-04-10 12:50:21 +0530

    Added localStorage checks for progress on gamescreen 1

--- Non modular game/js/levelTracker.js
+++ Non modular game/js/levelTracker.js
@@ -1,17 +1,49 @@
 $(document).ready(function () {
+    var stats;
     if (typeof (localStorage.getItem("stats")) == 'undefined') {
-        var stats = JSON.parse(localStorage.stats);
+        stats = JSON.parse(localStorage.stats);
     } else {
-        //init() here
+        //init() here. Only for testing
         var stats = {
-            stagesCrossed: [1],
+            stagesCrossed: [],
             stages: {
-                "1": {
-                    attempts: 1,
-                    treats: 5,
-                    perfect: true
-                }
             }
         };
+        localStorage.stats = JSON.stringify(stats);
     }
+
+    if (window.location.search !== "") {
+        var stage = window.location.search.split("stage=")[1].split("&")[0];
+        var stats = JSON.parse(localStorage.stats);
+        console.log(stats);
+        if (!(stage in stats.stages)) {
+            stats.stages[stage] = {
+                attempts: 0,
+                treats: 0,
+                perfect: false
+            };
+        }
+        stats.stages[stage].attempts += 1;
+        localStorage.stats = JSON.stringify(stats);
+    }
+
+    $(document).on('stageCompletion', function (event, treats, maxTreats) {
+        var stats = JSON.parse(localStorage.stats);
+        var stage = window.location.search.split("stage=")[1].split("&")[0];
+        //First attempt
+        if (stats.stages[stage].attempts === 1)
+        {
+            stats.stages[stage].treats = treats;
+            stats.stages[stage].perfect = (treats === maxTreats);
+            if (treats === maxTreats) {
+                if ($.inArray(parseInt(stage), stats.stagesCrossed) === -1)
+                    stats.stagesCrossed.push(parseInt(stage));
+            }
+            localStorage.stats = JSON.stringify(stats);
+        }
+    });
+
+    window.onbeforeunload = function () {
+        return "If you do that, you will not be eligible to be promoted anymore! Press 'Don't reload'!";
+    };
 });
